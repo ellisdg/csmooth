FIX: fixes mae comparison for gradient descent

csmooth/optimization.py
@@ -70,6 +70,11 @@ def find_optimal_tau(fwhm, edge_src, edge_dst, edge_distances, shape, initial_ta
                                    max_iterations=max_iterations, stop_threshold=stop_threshold,
                                    learning_rate=learning_rate/2, decay_rate=decay_rate,
                                    random_seed=random_seed, log_time=False, start_iteration=i+1)
+            smoothed_noise = heat_kernel_smoothing(signal_data=random_noise, tau=tau, edge_src=edge_src,
+                                                   edge_dst=edge_dst, edge_distances=edge_distances)
+            current_fwhm = estimate_fwhm(edge_src=edge_src, edge_dst=edge_dst,
+                                         edge_distances=edge_distances, signal_data=smoothed_noise)
+            mae = np.abs(current_fwhm - fwhm)
             break
         if mae < stop_threshold:
             break
@@ -90,9 +95,7 @@ def find_optimal_tau(fwhm, edge_src, edge_dst, edge_distances, shape, initial_ta
 
     end_time = time.time()
     if log_time:
-        # TODO: fix logging the number of iterations when recursion happens. Right now it logs the iterations from the last recursion
-        logger.info(f"Gradient descent completed in {i + 1} iterations.")
-        # TODO: fix current_fwhm when recursion happens. Right now it logs the fwhm from the last iteration of the last recursion
+        logger.info(f"Gradient descent completed.")
         logger.info(f"Final tau: {tau:.2f}, "
                      f"achieved fwhm: {current_fwhm:.2f}, "
                      f"target fwhm: {fwhm:.2f}, "